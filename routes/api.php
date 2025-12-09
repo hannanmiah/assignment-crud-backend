202f97b4ba4f1512b9d6c1b4feb50809eca74bfe
Hannan Miah <hannanhridoy@gmail.com>
2025-12-09 22:45:27 +0600

    chore: initial commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('auth')->name('auth.')->controller(AuthenticationController::class)->group(function () {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+    Route::post('logout', 'logout')->middleware('auth:sanctum')->name('logout');
+});
